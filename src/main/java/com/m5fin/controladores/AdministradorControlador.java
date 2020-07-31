f6d8f045cefcf89290dd8d406726114e0571e6a0
Daniel Zamora <danielz.informatico@gmail.com>
2020-07-31 19:06:34 -0400

    actualizando administrador

--- src/main/java/com/m5fin/controladores/AdministradorControlador.java
+++ src/main/java/com/m5fin/controladores/AdministradorControlador.java
@@ -9,8 +9,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
